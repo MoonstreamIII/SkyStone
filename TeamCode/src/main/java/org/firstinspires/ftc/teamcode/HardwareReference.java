5a2c6866513a8a05192bd61092592b4b472a1de8
IsaacTheAwesome <isaac.saff@gmail.com>
2019-09-12 15:56:34 -0400

    This is interesting.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareReference.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareReference.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 public class HardwareReference {
-
+    //This is a comment. YAY!
     public static final String LEFT_FRONT_DRIVE = "";
     public static final String LEFT_REAR_DRIVE = "";
     public static final String RIGHT_FRONT_DRIVE = "";
