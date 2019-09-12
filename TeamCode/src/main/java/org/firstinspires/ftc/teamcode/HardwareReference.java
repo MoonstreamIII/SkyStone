70526aeb7f85f11476439e445d3bef63368136a0
Yodude2002 <Yococommand@gmail.com>
2019-09-12 15:53:06 -0400

    Adding HardwareReference

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareReference.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public class HardwareReference {
+
+    public static final String LEFT_FRONT_DRIVE = "";
+    public static final String LEFT_REAR_DRIVE = "";
+    public static final String RIGHT_FRONT_DRIVE = "";
+    public static final String RIGHT_REAR_DRIVE = "";
+}
