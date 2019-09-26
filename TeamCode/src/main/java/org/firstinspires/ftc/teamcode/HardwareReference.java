56069ad3c19e84466c41612c87228df5e2e32351
IsaacTheAwesome <isaac.saff@gmail.com>
2019-09-26 15:12:27 -0400

    THINGS.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareReference.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareReference.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 public class HardwareReference {
-    //This is a comment. YAY!
+    //These are currently blank, but the strings, when filled, will tell the robot phone the motor corresponding to the name.
+    //TODO:Fill these out
+    //TODO:Add definitions for arm parts
     public static final String LEFT_FRONT_DRIVE = "";
     public static final String LEFT_REAR_DRIVE = "";
     public static final String RIGHT_FRONT_DRIVE = "";
