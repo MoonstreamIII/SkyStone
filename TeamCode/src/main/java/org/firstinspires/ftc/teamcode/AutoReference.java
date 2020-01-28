e6fc0fae221a4c2d94ba10996a3f390bdf339566
IsaacTheAwesome <isaac.saff@gmail.com>
2020-01-28 14:17:52 -0500

    THINGS.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoReference.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoReference.java
@@ -15,6 +15,20 @@ public class AutoReference {
         public static final double power = 0.5;
         public static final double firstLegMultiplier = 1;
     }
+    public class UnderBridgeEncoder {
+        public static final double leg1 = 0.25;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
+    public class FarBridgeEncoder {
+        public static final double leg1 = 0.75;
+        public static final double leg2 = 1.5;
+        public static final double leg3 = 0.0;
+        public static final double power = 0.5;
+        public static final double firstLegMultiplier = 1;
+    }
     //BrandonStuff
 
 }
